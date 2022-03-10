3b697b84edec8a79ea7211193e2874e7a5e92355
Fallen20 <montsegordillo20@hotmail.com>
2022-03-10 13:42:50 +0100

    calculator

---
+++ app/src/main/java/com/example/calculator/Calculos.java
@@ -0,0 +1,10 @@
+package com.example.calculator;
+
+public class Calculos {
+
+    public double suma(double num1, double num2){return num1+num2;}
+    public double resta(double num1, double num2){return num1-num2;}
+    public double multi(double num1, double num2){return num1*num2;}
+    public double div(double num1, double num2){return num1/num2;}
+    public double pot(double num1, double num2){return Math.pow(num1,num2);}
+}
